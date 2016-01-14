a49945cd58e8aa76c8d33e747b059cc8daf92984
Mohan Dhar <mohandhar.md@gmail.com>
2016-01-14 15:18:17 -0500

    Creating an example project where I will interpolate objective C and Swift just so I have something to refer to later

---
+++ VideoToGifPrototype/VideoToGifPrototype/bridging-header.h
@@ -0,0 +1,12 @@
+//
+//  bridging-header.h
+//  VideoToGifPrototype
+//
+//  Created by Mohan Dhar on 1/12/16.
+//  Copyright © 2016 Mohan Dhar. All rights reserved.
+//
+
+#ifndef bridging_header_h
+#define bridging_header_h
+
+#endif /* bridging_header_h */
